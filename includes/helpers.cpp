5a92b6872ed3046073244a8a90eae8f26fc4a8c1
augustinavicius-vu <ignas.augustinavicius@mif.stud.vu.lt>
2023-05-09 20:53:18 +0200

    Programos spartos testavimo pradinis funkcionalumas

--- includes/helpers.cpp
+++ includes/helpers.cpp
@@ -85,15 +85,95 @@ void GeneruotiFaila(int studentuSk, int namuDarbuSk, std::string vieta)
     failas.close();
 }
 
+void NuskaityiFaila(VStudentas studentai, std::string vieta)
+{
+    std::ifstream failas(vieta);
+
+    int laukelis = -1;
+    int namuDarbuSk = 0;
+    bool skaityti = false;
+
+    std::string tekstas;
+    std::string tempVardas;
+    std::string tempPavarde;
+    int tempEgzaminasRez;
+    std::vector<int> tempNamuDarbaiRez;
+    while (failas >> tekstas)
+    {
+        if (skaityti)
+        {
+            laukelis++;
+
+            // Vardas
+            if (laukelis == 0)
+            {
+                tempVardas = tekstas;
+                continue;
+            }
+
+            // Pavarde
+            if (laukelis == 1)
+            {
+                tempPavarde = tekstas;
+                continue;
+            }
+
+            // Namu Darbai
+            if (laukelis > 1 && laukelis < 2 + namuDarbuSk)
+            {
+                tempNamuDarbaiRez.push_back(std::stoi(tekstas));
+                continue;
+            }
+
+            // Egzaminas
+            if (laukelis > 1 + namuDarbuSk)
+            {
+                tempEgzaminasRez = std::stoi(tekstas);
+                //NaujasStudentas(studentoIndeksas, studentai, tempVardas, tempPavarde, tempEgzaminasRez, tempNamuDarbaiRez);
+
+                laukelis = -1;
+                continue;
+            }
+
+            
+
+
+        } else {
+            if (tekstas == "Egzaminas")
+            {
+                skaityti = true;
+                namuDarbuSk = namuDarbuSk - 2;
+            } else {
+                namuDarbuSk++;
+            }
+        }
+    }
+
+    failas.close();
+}
+
 void TestuotiSparta(int irasuSk, int namuDarbuSk, std::string vieta)
 {
     std::cout << "Pradedama programos spartos analizė" << std::endl;
 
+    // Failo generavimas
     auto startGen = std::chrono::high_resolution_clock::now();
     GeneruotiFaila(irasuSk, namuDarbuSk, vieta);
     auto endGen = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> diffGen = endGen - startGen;
-    std:: cout << irasuSk << " įrašų generavimas ir išvedimas į failą truko: " << diffGen.count() << std::endl;
+    std::cout << irasuSk << " įrašų generavimas ir išvedimas į failą truko: " << diffGen.count() << std::endl;
+
+    // Failo nuskaitymas
+    VStudentas studentai;
+    auto startRead = std::chrono::high_resolution_clock::now();
+    NuskaityiFaila(studentai, vieta);
+    auto endRead = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> diffRead = endRead - startRead;
+    std::cout << irasuSk << " įrašų nuskaitymas truko: " << diffRead.count() << std::endl;
+
+    
+
+
 
     Pauze();
 
