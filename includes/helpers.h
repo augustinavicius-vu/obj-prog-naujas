64afa8817160213f16cb63e37c40198d7a42eaa5
augustinavicius-vu <ignas.augustinavicius@mif.stud.vu.lt>
2023-05-15 19:10:57 +0200

    Big fix

--- includes/helpers.h
+++ includes/helpers.h
@@ -16,5 +16,5 @@ void RikiuotiStudentus(VStudentas &studentai);
 void RusiuotiStudentusBalas(VStudentas &studentai, VStudentas &studentaiA, VStudentas &studentaiB, int &studentaiASk, int &studentaiBSk, int &studentoIndeksas);
 
 void GeneruotiFaila(int studentuSk, int namuDarbuSk, std::string vieta);
-void GeneruotiFailaBalas(int studentuSk, VStudentas studentai, std::string vieta)
+void GeneruotiFailaBalas(int studentuSk, VStudentas studentai, std::string vieta);
 void TestuotiSparta(int irasuSk, int namuDarbuSk, std::string vieta, int &studentoIndeksas, VStudentas &studentai);
