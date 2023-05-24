394c131898bc0e27e0de37f4a69676456d95b388
augustinavicius-vu <ignas.augustinavicius@mif.stud.vu.lt>
2023-05-24 13:11:02 +0200

    Panaikintos neberaikalingos funkcijos

--- includes/functions.h
+++ includes/functions.h
@@ -15,20 +15,3 @@ int AtsitiktinisSkaicius(int min, int max);
 // Funkcijos
 void GeneruotiFaila(int studentuSk, int namuDarbuSk, std::string vieta);
 void EksportuotiStudentus(StudentasVector studentai, std::string vieta);
-
-
-/*
-// Pagalbinės funkcijos
-template <typename Container>
-void NaujasStudentas(int &studentoIndeksas, Container &studentai, std::string vardas, std::string pavarde, int egzaminasRez, std::vector<int> namuDarbaiRez);
-
-template <typename Container>
-void RikiuotiStudentus(Container &studentai);
-
-template <typename Container>
-void RusiuotiStudentusBalas(Container &studentai, Container &studentaiA, Container &studentaiB, int &studentaiASk, int &studentaiBSk);
-
-void GeneruotiFailaBalas(int studentuSk, VStudentas studentai, std::string vieta);
-void TestuotiSparta(int irasuSk, int namuDarbuSk, std::string vieta, int &studentoIndeksas, VStudentas &studentai);
-void TestuotiKonteinerius(std::string vieta);
-*/
--- includes/menu.h
+++ includes/menu.h
@@ -8,7 +8,3 @@ void PasirinkimasGeneruotiStudentuFaila();
 void PasirinkimasRikiuotiStudentus(StudentasVector &studentai);
 void PasirinkimasRusiuotiStudentus(StudentasVector &studentai, StudentasVector &studentaiGalv, StudentasVector &studentaiVarg);
 void PasirinkimasEksportuotiStudentus(StudentasVector &studentai);
-//void PasirinkimasSkaiciuotiAbu(VStudentas &studentai);
-//void PasirinkimasSkaiciuotiVidurki(VStudentas &studentai);
-//void PasirinkimasSkaiciuotiMediana(VStudentas &studentai);
-//void PasirinkimasGreicioAnalize(int &studentoIndeksas, VStudentas &studentai);
