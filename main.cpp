0cb0a34a9263691ba8a52fd351cbe379bd197b0c
augustinavicius-vu <ignas.augustinavicius@mif.stud.vu.lt>
2023-05-26 10:09:23 +0200

    papildomi testai

--- main.cpp
+++ main.cpp
@@ -8,22 +8,25 @@
 int main()
 {
     Studentas s1;
-    Studentas s2;
-
-    // Ivestis
     std::cin >> s1;
-    std::cin >> s2;
 
-    // Isvestis
-    std::cout << s1 << std::endl;
+    // Kopijos konstruktorius
+    Studentas s2(s1);
     std::cout << s2 << std::endl;
 
+    // Perkėlimo konstruktorius
+    Studentas s3(std::move(s1));
+    std::cout << s3 << std::endl;
 
-    s1 = s2;
-    std::cout << s1 << std::endl;
-    std::cout << s2 << std::endl;
+    // Kopijos prisikirimo operatorius
+    Studentas s4;
+    s4 = s2;
+    std::cout << s4 << std::endl;
 
-
+    // Perkėlimo prisikirimo operatorius
+    Studentas s5;
+    s5 = std::move(s3);
+    std::cout << s5 << std::endl;
 
 
     /*
